f490bcd80f16b7fdfeefaaf34cc325e1f5bb5da1
Lewi1006 <coach.wind11@gmail.com>
2026-05-18 22:42:43 +0200

    feat: book card for loop

---
+++ script.js
@@ -0,0 +1,14 @@
+//  display book cards when page is loaded --> onload
+function init(){
+    
+}
+
+
+// 
+function renderBookCard() {
+    let bookCardRef = document.getElementById("book-card");
+    bookCardRef.innerHTML = "";
+    for (let indexBooks = 0; indexBooks < books.length; indexBooks++){
+        bookCardRef.innerHTML += getBookTemplate(indexBook)
+    } 
+}
