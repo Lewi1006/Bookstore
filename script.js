871ecfaadd54b8ac750abae59296c92aec92bcf5
Lewi1006 <coach.wind11@gmail.com>
2026-05-20 22:25:44 +0200

    feat: local storage

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
 //  display book cards when page is loaded --> onload
-function init(){
-    renderBookCard();
+function init() {
+  getFromLocalStorage();
+  renderBookCard();
 }
 
-
-// 
+//
 function renderBookCard() {
-    let bookCardRef = document.getElementById("book-card");
-    bookCardRef.innerHTML = "";
-    for (let indexBooks = 0; indexBooks < books.length; indexBooks++){
-        bookCardRef.innerHTML += getBookTemplate(indexBooks);
-    } 
+  let bookCardRef = document.getElementById("book-card");
+  bookCardRef.innerHTML = "";
+  for (let indexBooks = 0; indexBooks < books.length; indexBooks++) {
+    bookCardRef.innerHTML += getBookTemplate(indexBooks);
+  }
 }
-
 
 // when button is clicked (heart is already red/liked = true) --> then it turns false and count goes down and vive versa
-function countLike(indexBooks){
-    if(books[indexBooks].liked === true){
-        books[indexBooks].liked = false;
-        books[indexBooks].likes--;
+function countLike(indexBooks) {
+  if (books[indexBooks].liked === true) {
+    books[indexBooks].liked = false;
+    books[indexBooks].likes--;
+  } else if (books[indexBooks].liked === false) {
+    books[indexBooks].liked = true;
+    books[indexBooks].likes++;
+  }
 
-    } else if (books[indexBooks].liked === false){
-        books[indexBooks].liked = true;
-        books[indexBooks].likes++;
-    }
-
-        renderBookCard();
+  saveToLocalStorage();
+  renderBookCard();
 }
 
+function addComment(indexBooks) {
+  let commentsInputRef = document.getElementById(`comments_input${indexBooks}`);
+
+  let newComment = commentsInputRef.value;
+  books[indexBooks].comments.push({ name: "User", comment: newComment });
 
 
-function addComment(indexBooks){
-let commentsInputRef = document.getElementById(`comments_input${indexBooks}`);
+  commentsInputRef.value = "";
 
-let newComment = commentsInputRef.value; 
-books[indexBooks].comments.push({name: "User", comment: newComment});
+  saveToLocalStorage();
+  renderBookCard();
 
-renderBookCard();
-
-commentsInputRef.value = "";
 }
 
+function saveToLocalStorage() {
+  localStorage.setItem("books", JSON.stringify(books));
+}
 
+function getFromLocalStorage() {
+  const storedBooks = localStorage.getItem("books");
+  let myArrBooks = JSON.parse(storedBooks);
 
-// &#9829;
-
-// function renderComments(){
-//     // let commentsRef = document.getElementById("comments");
-//     // commentsRef.innerHTML = "";
-//     // for(let indexComments = 0; indexComments < books[indexBooks].comments.length; indexComments++){
-//     //     commentsRef.innerHTML += getCommentsTemplate(indexComments);
-//     // }
-// }
+  if (myArrBooks !== null) {
+    books = myArrBooks;
+  }
+}
